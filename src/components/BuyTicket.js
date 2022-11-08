441cc41b69fdfdf5516ba57164dd31773ce44179
Andrey Bliznev <majeretft@mail.ru>
2022-11-08 16:26:54 +0300

    Format code and get back header caption

--- src/components/BuyTicket.js
+++ src/components/BuyTicket.js
@@ -1,60 +1,61 @@
-import React, {useState} from 'react';
-import styles from "./Films.scss"
+import React, { useState } from "react";
+import styles from "./Films.scss";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import Form from "./Form";
 
-
 const Films = () => {
-    const [seats, setSeats] = React.useState([])
-    const [loading, setLoading] = useState(true)
+  const [seats, setSeats] = React.useState([]);
+  const [loading, setLoading] = useState(true);
 
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("http://127.0.0.1:8000/api/v1/seats/");
+        console.log(data);
 
-    React.useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const {data} = await axios.get(
-                    'http://127.0.0.1:8000/api/v1/seats/'
-                )
-                console.log(data)
+        setSeats(data);
+      } catch (error) {
+        console.log(error);
+      }
 
-                setSeats(data)
-            } catch (error) {
-                console.log(error)
-            }
+      setLoading(false);
+    };
 
-            setLoading(false)
-        }
+    fetchData();
+  }, []);
 
-        fetchData()
-    }, [])
+  return (
+    <div style={{ display: "block" }}>
+      {loading && "Загрузка..."}
 
+      <div
+        style={{
+          display: "flex",
+          overflow: "auto",
+        }}
+      >
+        <p>Выбрать место: </p>
+        {seats.map((el) => (
+          <div key={el.id} className={styles.card} style={{ margin: 2 }}>
+            <Button variant="outline-primary" style={{}}>
+              {el.id}
+            </Button>
+          </div>
+        ))}
+      </div>
 
-    return (
-        <div style={{display: 'block'}}>
-            {loading && 'Загрузка...'}
+      <div
+        style={{
+          display: "flex",
+          overflow: "auto",
+        }}
+      >
+        <p>Ввести данные:</p>
+      </div>
+      <Form />
+    </div>
+  );
+};
 
-
-            <div style={{
-                display: 'flex',
-                overflow: 'auto',
-            }}>
-                <p>Выбрать место: </p>
-                {seats.map(el => (
-                    <div key={el.id} className={styles.card} style={{margin: 2}}>
-                        <Button variant="outline-primary" style={{}}>{el.id}</Button>
-                    </div>
-                ))}
-            </div>
-
-            <div style={{
-                display: 'flex',
-                overflow: 'auto'
-            }}>
-                <p>Ввести данные:</p></div>
-            <Form/>
-        </div>
-    )
-}
-
-export default Films
+export default Films;
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
       <Container>
         <Navbar.Brand as={Link} to="/">
           <img src={logoImage} alt="" height="50" />
+          <h2 className="d-inline-block mb-0 ms-2 align-middle" style={{ height: `50px` }}>Кинотеатр Бауманец</h2>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
